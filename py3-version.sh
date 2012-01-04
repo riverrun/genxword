4b6fe9f2bd0a24dbb124a548d43f1bd0ece03f0d
David Whitlock <alovedalongthe@gmail.com>
2012-01-04 19:57:06 +0700

    Improved py3 support for new version

--- py3-version.sh
+++ py3-version.sh
@@ -4,3 +4,4 @@ sed -i '1s/python2.7/python3/' setup.py
 sed -i '1s/python2.7/python3/' genxword
 sed -i '1s/python2.7/python3/' gencrossword/gencrossword.py
 sed -i 's/raw_input/input/g' gencrossword/gencrossword.py
+sed -i 's/raw_input/input/g' gencrossword/calcxword.py
