4f506bfd9ddd488e5100e981897173cee57e3dc3
David Whitlock <alovedalongthe@gmail.com>
2012-02-19 10:04:48 +0700

    Further update to install script

--- install_genxword.sh
+++ install_genxword.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Script to install genxword
+# Script to install genxword, which depends on Python (2.7 or 3) and Pycairo.
+# This script does not check for dependencies. That's your job.
+
+set -e
+ERROR_MESSAGE="$(tput bold)$(tput setaf 1)An error occurred. \
+Please read the output above to see what the problem is."
 
 read -p "Just press Enter to use the Python2.7 version. \
 To use the Python3 version, press any key and then Enter."
@@ -9,10 +14,10 @@ if [[ -n $REPLY ]]; then
     sed -i '1s/python2.7/python3/' gencrossword/gencrossword.py
     sed -i 's/raw_input/input/g' gencrossword/gencrossword.py
     sed -i 's/raw_input/input/g' gencrossword/calcxword.py
-    python3 setup.py install
+    python3 setup.py install || { echo -e $ERROR_MESSAGE; exit 1; }
     APP_NAME=genxword-py3
 else
-    python2.7 setup.py install
+    python2.7 setup.py install || { echo -e $ERROR_MESSAGE; exit 1; }
     APP_NAME=genxword
 fi
 echo "Installing the $APP_NAME program"
