ac435884f6abe0f085071a5164331daad6e68c41
David Whitlock <alovedalongthe@gmail.com>
2012-03-05 20:30:10 +0700

    Updated convert script

--- convert_dict.sh
+++ convert_dict.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-# Script to convert a dictionary file into a file that can be used with genxword
+# Script to convert a word list file into a file that can be used with genxword
+# This script will produce a file with words 3-11 words long
+# and if there are any clues it will print them as well.
 
-awk 'length($1) > 2 && length($1) < 12' $1 > $1_new.txt
+awk '{
+if ($1 ~ "^[[:alpha:]]+$" && a[tolower($1)]++ == 0 && length($1) > 2 && length($1) < 12)
+    print $0;
+}' "$@" > "$@".new
