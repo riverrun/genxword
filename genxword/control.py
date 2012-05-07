193f6f3d80fda95e489c7dfb6e8639700ca04cca
David Whitlock <alovedalongthe@gmail.com>
2012-05-07 15:46:25 +0700

    More work error handling

--- genxword/control.py
+++ genxword/control.py
@@ -40,7 +40,7 @@ class Genxword(object):
         try:
             self.word_list = [[word[0].upper(), word[1]] for word in word_list]
         except:
-            self.word_list = [[word[0].upper(), 'Write clue for ' + word[0]] for word in self.word_list]
+            self.word_list = [[word[0].upper(), 'Write clue for ' + word[0]] for word in word_list]
         self.word_list.sort(key=lambda i: len(i[0]), reverse=True)
 
     def grid_size(self, gtkmode=False):
