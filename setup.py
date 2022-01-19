197050c55f113bbab9f1f30049455a12088b76f1
David Whitlock <alovedalongthe@gmail.com>
2022-01-19 10:42:48 +0700

    Update version to 2.2.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name='genxword',
-    version='2.1.0',
+    version='2.2.0',
     author='David Whitlock',
     author_email='alovedalongthe@gmail.com',
     url='https://github.com/riverrun/genxword',
